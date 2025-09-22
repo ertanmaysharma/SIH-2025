c7168a17b6362bb4bcce624df758f431b365bf7f
ertanmaysharma <ertanmaysharma@gmail.com>
2025-09-22 18:37:26 +0530

    Delete app.js

--- app.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('studentBtn').onclick = () => location.href = 'job_application.html';
-    document.getElementById('companyBtn').onclick = () => location.href = 'company_registration.html';
-});
