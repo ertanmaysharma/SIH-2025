3d1c5c21e75e89907ad5bed601c7692e0d758d54
ertanmaysharma <ertanmaysharma@gmail.com>
2025-09-22 18:15:10 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('studentBtn').onclick = () => location.href = 'job_application.html';
+    document.getElementById('companyBtn').onclick = () => location.href = 'company_registration.html';
+});
